98a44fece3397c4d97cc04da3905361e869760e8
Natalia001-bz <125935088+Natalia001-bz@users.noreply.github.com>
2023-05-27 23:11:41 -0500

    configuracion nodejs

---
+++ server.js
@@ -0,0 +1,7 @@
+import express from "express";
+import "dotenv/config.js";
+const server  = express();
+const PORT = process.env.PORT || 8080;
+const ready = () => console.log("ready on: " + PORT);
+server.listen(PORT, ready);
+console.log(process.env.NODE_ENV);
