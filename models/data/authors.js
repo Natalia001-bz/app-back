16fd7d65cd3bae0927e3725d0a6f38b0bf4a1efc
Natalia <125935088+Natalia001-bz@users.noreply.github.com>
2023-05-28 22:51:29 -0500

    moddelo de autores (#2)

---
+++ models/data/authors.js
@@ -0,0 +1,45 @@
+import 'dotenv/config.js';
+import '../../config/database.js';
+import Author from '../Author.js';
+
+let authors = [{
+    name: "alejandro",
+    city: "buenos aires",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true,
+    user_id: '64740d7b5986633381f043e8'
+}, {
+    name: "lucas",
+    last_name: "silva",
+    city: "buenos aires",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true,
+    user_id: '64740d7b5986633381f043e9'
+}, {
+    name: "jose",
+    last_name: "lopez",
+    city: "villa carlos paz",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true,
+    user_id: '64740d7b5986633381f043ea'
+}, {
+    name: "eric",
+    city: "resistencia",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true,
+    user_id: '64740d7b5986633381f043eb'
+}, {
+    name: "igna",
+    last_name: "borraz",
+    city: "rosario",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true,
+    user_id: '64740d7b5986633381f043ec'
+}];
+
+Author.insertMany(authors);
