44d863d1df35c38c71985a688092c7bc7a7a7f77
Natalia001-bz <125935088+Natalia001-bz@users.noreply.github.com>
2023-05-28 20:28:30 -0500

    minga 03

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 import express from "express";
 import "dotenv/config.js";
+import './config/database.js';
+
 const server  = express();
 const PORT = process.env.PORT || 8080;
 const ready = () => console.log("ready on: " + PORT);
+
+//Middlewares
+server.use(express.json());
+server.use(express.urlencoded({extended: false}));
+
+//router+server
 server.listen(PORT, ready);
 console.log(process.env.NODE_ENV);
